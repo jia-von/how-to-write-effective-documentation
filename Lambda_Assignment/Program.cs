49619aa4fd072e9c07995140f5b0178502ae7b00
Jia <jiavon@ualberta.ca>
2020-09-15 21:02:06 -0600

    update code

--- Lambda_Assignment/Program.cs
+++ Lambda_Assignment/Program.cs
@@ -7,14 +7,6 @@ namespace Lambda_Assignment
     {
         static void Main(string[] args)
         {
-            // Example 1 (Microsoft, 2015a): 
-            // Initialization of an anonymous method with the use of delegate.
-            TestDelegate testDelB = delegate (string s) { Console.WriteLine(s); };
-            
-
-            // Example 2 (Microsoft, 2015a): 
-            // Initialization of an anonymous method with the use of an lambda expression.
-            TestDelegate testDelC = (x) => { Console.WriteLine(x); };
             
 
             // Example 3:
@@ -23,14 +15,11 @@ namespace Lambda_Assignment
             
 
             //Invoke the delegates
-            testDelB("Print testDelB");
-            testDelC("Print testDelC");
             Console.WriteLine(test(5, 5));
+
+            //Output:
+            //10
         }
-
-        // Example 1: Declaration of method with a string parameter s
-        delegate void TestDelegate(string s);
-
         // Example 3: 
         // Step 1: Make a delegate.
         delegate int Example3(int x, int y);
