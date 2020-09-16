aaf825460263f37564ad2ac769da427d3e7707cf
Jia <jiavon@ualberta.ca>
2020-09-15 20:20:43 -0600

    fixed the code with c# coding convention.

--- Lambda_Assignment/Program.cs
+++ Lambda_Assignment/Program.cs
@@ -14,14 +14,14 @@ namespace Lambda_Assignment
             testDelB("Print testDelB");
 
             // Example 2 (Microsoft, 2015a): 
-            //Initialization of an anonymous method with the use of an lambda expression.
-            //Invoke the delegate testDelC
+            // Initialization of an anonymous method with the use of an lambda expression.
+            // Invoke the delegate testDelC
             TestDelegate testDelC = (x) => { Console.WriteLine(x); };
             testDelC("Print testDelC");
 
-            //Example 3:
-            //Step 2: Initialize Anonymous function()
-            //Step 3: Invoke the delegate 
+            // Example 3:
+            // Step 2: Initialize Anonymous function()
+            // Step 3: Invoke the delegate 
             Example3 test = (x, y) => x + y;
             Console.WriteLine(test(5, 5));
 
@@ -31,8 +31,8 @@ namespace Lambda_Assignment
         // Example 1: Declaration of method with a string parameter s
         delegate void TestDelegate(string s);
 
-        //Example 3: 
-        //Step 1: Make a delegate.
+        // Example 3: 
+        // Step 1: Make a delegate.
         delegate int Example3(int x, int y);
     }
 }
